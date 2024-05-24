820aacb8ec3b9470f3214b2438ff07265375cacf
Brandon <brandonroulstone71@gmail.com>
2024-05-24 15:49:02 +0200

    initial commit

--- new_portfolio/app/home/page.js
+++ new_portfolio/app/home/page.js
@@ -1,43 +1,17 @@
+"use client"
 import React from 'react'
+import { TypeAnimation } from 'react-type-animation';
 
 const homePage = () => {
   return (
-    <>
-        <div className="stats shadow">
-        
-            <div className="stat">
-                <div className="stat-figure text-primary">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
-                </div>
-                <div className="stat-title">Total Likes</div>
-                <div className="stat-value text-primary">25.6K</div>
-                <div className="stat-desc">21% more than last month</div>
-            </div>
-            
-            <div className="stat">
-                <div className="stat-figure text-secondary">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
-                </div>
-                <div className="stat-title">Page Views</div>
-                <div className="stat-value text-secondary">2.6M</div>
-                <div className="stat-desc">21% more than last month</div>
-            </div>
-            
-            <div className="stat">
-                <div className="stat-figure text-secondary">
-                <div className="avatar online">
-                    <div className="w-16 rounded-full">
-                    <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-                    </div>
-                </div>
-                </div>
-                <div className="stat-value">86%</div>
-                <div className="stat-title">Tasks done</div>
-                <div className="stat-desc text-secondary">31 tasks remaining</div>
-            </div>
-            
-        </div> 
-    </>
+    <div className="hero-content flex-col lg:flex-row-reverse">
+    <Image src="https://cdn-images.imagevenue.com/dd/3f/2d/ME18C3BP_o.jpg" width="550" height="550" className="rounded-lg min-[100vw] shadow-2xl lg:max-w-lg" />
+    <div>
+      <h1 className="text-3xl font-bold text-white lg:text-6xl">Lorem ipsum <span className="text-indigo-700">dolor!</span> </h1>
+      <p className="py-3 lg:py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <button className="btn btn-primary">Get Started</button>
+    </div>
+  </div>
   )
 }
 
--- new_portfolio/app/page.js
+++ new_portfolio/app/page.js
@@ -2,12 +2,15 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-row items-center justify-center p-0 bg-zinc-900">
-      <div className="xl:container flex flex-col lg:text-6xl sm:text-4xl">
-        <h1 className="lg:text-white uppercase text-start mb-5">Hello I am</h1>
-        <h1 className="text-white uppercase text-start">Brandon Roulstone <span className="text-indigo-700">.</span></h1>
+    <div className="hero min-h-screen bg-zinc-900">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <Image src="https://cdn-images.imagevenue.com/dd/3f/2d/ME18C3BP_o.jpg" width="550" height="550" className="rounded-lg min-[100vw] shadow-2xl lg:max-w-lg" />
+        <div>
+          <h1 className="text-3xl font-bold text-center text-white lg:text-6xl lg:text-start">Lorem ipsum <span className="text-indigo-700">dolor!</span> </h1>
+          <p className="py-3 text-start lg:py-6 lg:text-start">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+          <button className="btn btn-primary">Get Started</button>
+        </div>
       </div>
-        <Image height={500} width={500} src="https://cdn-images.imagevenue.com/dd/3f/2d/ME18C3BP_o.jpg" className="absolute right-0 h-screen object-cover grayscale" alt="My_Image"/>
     </div>
   );
 }
